e89d780b1a8dbfef8aad1916fec8143a2d66f750
Ryan Bangoy <rcbangoy@outlook.com>
2020-02-04 00:08:23 +0800

    Refactor 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Xml;
-using Grpc.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using opg_201910_interview.Models;
 
 namespace opg_201910_interview.Controllers
