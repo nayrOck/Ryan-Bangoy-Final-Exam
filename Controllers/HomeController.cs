e7ae1ed58d2f191f11dbb9c43f59ca7e5a8a2cad
nayrOck <30563673+nayrOck@users.noreply.github.com>
2020-02-03 20:19:45 +0800

    Refactor 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml;
+using Grpc.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -15,77 +18,70 @@ namespace opg_201910_interview.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly IHostingEnvironment _host;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IHostingEnvironment hosting)
         {
-            _logger = logger;
+            _host = hosting;
         }
 
         public IActionResult Index()
         {
-            var c = new Client();
+            List<Client> clients = new List<Client>();
+
+            return View(clients);
+        }
+
+        [HttpPost]
+        //[ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        public IActionResult Index(Client client)
+        {
             List<Client> clients = new List<Client>();
             List<FileMode> jsonfiles = new List<FileMode>();
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\rb\Desktop\FlexisourceIT_DotnetCoreExam\opg-201910Base-master\opg-201910Base-master\UploadFiles\ClientA");
+            DirectoryInfo d = new DirectoryInfo(_host.ContentRootPath + @"\UploadFiles\" + client.ClientList);
             int s = 1;
-            
+
             if (d.Exists)
             {
-
                 FileInfo[] files = d.GetFiles("*.xml");
                 var obj = files.ToList();
                 foreach (FileInfo file in files)
                 {
+                    var iFile = new ImportUtility();
                     string fileName = Path.GetFileNameWithoutExtension(file.Name);
-                    var x = c.ValidateFormat(d.Name, fileName);
+                    var x = iFile.ValidateFilenameDate( fileName);
                     if (x.FileDate != null)
                     {
-                        var xj = (new {Client = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate });
-                        clients.Add(new Client { Sort = s, Name = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate, JSONFile = c.ConvertToJSON(xj) });
+                        var xj = (new { Client = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate });
+                        clients.Add(new Client { Sort = s, Name = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate, JSONFile = iFile.ConvertToJSON(xj), FileDateFormat = x.FileDateFormat });
                         s++;
 
                     }
-                }               
+                }
                 
             }
 
-            return View(clients.OrderBy(c => c.FileDate));
-
-        }
-
-
-        public IActionResult Privacy()
-        {
-            var c = new Client();
-            List<Client> clients = new List<Client>();
-            List<FileMode> jsonfiles = new List<FileMode>();
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\rb\Desktop\FlexisourceIT_DotnetCoreExam\opg-201910Base-master\opg-201910Base-master\UploadFiles\ClientB");
-            int s = 1;
-            if (d.Exists)
+            if (client.ClientList.ToString() == "ClientA")
             {
+                var outputFiles = clients.FindAll(c => c.FileDateFormat == (from item in clients
+                                                                            group item.FileDateFormat by item.FileDateFormat into g
+                                                                            orderby g.Count() descending
+                                                                            select g.Key).First().ToString()).OrderBy(n => Convert.ToDateTime(n.FileDate).ToString("yyyy")).ThenBy(c => Convert.ToDateTime(c.FileDate).ToString("MM"));
 
-                FileInfo[] files = d.GetFiles("*.xml");
-                var obj = files.ToList();
-                foreach (FileInfo file in files)
-                {
-                    string fileName = Path.GetFileNameWithoutExtension(file.Name);
+                return View(outputFiles);
+            }
+            else
+            {
+                var outputFiles = clients.FindAll(c => c.FileDateFormat == (from item in clients
+                                                                            group item.FileDateFormat by item.FileDateFormat into g
+                                                                            orderby g.Count() descending
+                                                                            select g.Key).First().ToString()).OrderByDescending(c => c.FileName);
 
-                    var x = c.ValidateFormat(d.Name, fileName);
-                    if (x.FileDate != null)
-                    {
-                        var xj = (new  { Sort = s, Client = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate });
-                        clients.Add(new Client { Sort = s, Name = d.Name, Id = x.Id, FileName = fileName, FileDirectoryPath = file.DirectoryName, FileDate = x.FileDate,JSONFile = c.ConvertToJSON(xj) });
-                        s++;
-
-                    }
-                    
-                }
-
+                return View(outputFiles);
             }
 
-            return View(clients.OrderBy(c => c.FileName));
-        }
+        }      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/Client.cs
+++ Models/Client.cs
@@ -15,60 +15,15 @@ namespace opg_201910_interview.Models
         public string FileName { get; set; }
         public string FileDirectoryPath { get; set; }
         public string FileDate { get; set; }
+        public string FileDateFormat { get; set; }
         public string JSONFile { get; set; }
+        public ClientList ClientList { get; set; }
+        
+    }
 
-        public Client ValidateFormat(string clientName, string fileName)
-        {
-
-            Client obj = new Client();
-            string _date;
-            DateTime dt;
-            string format = null;
-            if (clientName == "ClientA")
-            {
-                _date = fileName.Substring(fileName.Length - 10, 10);
-                format = "yyyy-MM-dd";
-
-            }
-            else
-            {
-                _date = fileName.Substring(fileName.Length - 8, 8);
-                format = "yyyyMMdd";
-
-            }
-
-            if (DateTime.TryParseExact(_date, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
-            {
-                if (clientName == "ClientA")
-                {
-                    _date = fileName.Substring(fileName.Length - 10, 10);
-                    format = "yyyy-MM-dd";
-                    obj.Id = "1001";
-                    obj.FileDate = _date;
-                    obj.Name = clientName;
-                    obj.FileName = fileName;
-                    return obj;
-                }
-                else
-                {
-                    _date = fileName.Substring(fileName.Length - 8, 8);
-                    format = "yyyyMMdd";
-                    obj.Id = "2001";
-                    obj.FileDate = DateTime.ParseExact(_date, "yyyyMMdd", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd");
-                    obj.Name = clientName;
-                    obj.FileName = fileName;
-                    return obj;
-                }
-            }
-            else
-                return obj;
-        }
-
-        public string ConvertToJSON(object list)
-        {
-            string sJSONResponse = JsonConvert.SerializeObject(list);
-
-            return sJSONResponse;
-        }
+    public enum ClientList
+    {
+        ClientA,
+        ClientB
     }
 }
